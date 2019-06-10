38ecd5f9c204c9eab7bbf98b3a109016aece5c28
Василий Бардюков <ferisefbns@gmail.com>
2019-06-10 18:13:07 +0300

    Init commit for Status

---
+++ src/components/Main/components/Status/index.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import filesize from 'filesize';
+
+import styles from './status.module.css';
+
+const { REACT_APP_API_URL = '' } = process.env;
+
+class Status extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null, error: '',
+    };
+  }
+
+  componentDidMount() {
+    this.updateStatus();
+    this.interval = setInterval(() => {
+      this.updateStatus();
+    }, 300000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  updateStatus() {
+    fetch(`${REACT_APP_API_URL}/api/status`, {
+      method: 'GET',
+      headers: {
+        accept: 'application/vnd.moosik.v1+json',
+        'content-type': 'application/json',
+      },
+    })
+      .then(r => r.json())
+      .then((data) => {
+        this.setState({ data });
+      })
+      .catch((error) => {
+        this.setState({ error });
+      });
+  }
+
+  render() {
+    const { data, error } = this.state;
+
+    return (
+      <section className={styles.status}>
+        <h1 className={styles.head}>Storage Status</h1>
+        {data && (
+          <div className={styles.body}>
+            <p className={styles.prop}>
+              Total Space:&ensp;
+              <span>{filesize(data.totalSpace)}</span>
+            </p>
+            <p className={styles.prop}>
+              Used Space:&ensp;
+              <span>{filesize(data.usedSpace)}</span>
+            </p>
+            <p className={styles.prop}>
+              Max File Size:&ensp;
+              <span>{filesize(data.maxFileSize)}</span>
+            </p>
+          </div>
+        )}
+        {error && <p className={styles.error}>{error}</p>}
+      </section>
+    );
+  }
+}
+
+export default Status;
