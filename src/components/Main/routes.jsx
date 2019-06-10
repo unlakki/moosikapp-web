f71b13c15d6ba8c32d92674f8ddd25d3e9edc65b
Василий Бардюков <ferisefbns@gmail.com>
2019-06-10 17:24:11 +0300

    Add paths

--- src/components/Main/routes.jsx
+++ src/components/Main/routes.jsx
@@ -1,3 +1,31 @@
-const routes = [];
+import React from 'react';
+
+const routes = [
+  {
+    path: '/',
+    component: () => <div>root</div>,
+    exact: true,
+  },
+  {
+    path: '/music',
+    component: () => <div>music</div>,
+  },
+  {
+    path: '/upload',
+    component: () => <div>upload</div>,
+  },
+  {
+    path: '/login',
+    component: () => <div>login</div>,
+  },
+  {
+    path: '/register',
+    component: () => <div>register</div>,
+  },
+  {
+    path: '/forgot',
+    component: () => <div>forgot</div>,
+  },
+];
 
 export default routes;
