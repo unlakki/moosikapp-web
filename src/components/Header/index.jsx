e479f539de4f09e2470fc54a288a7439b4dd4849
Василий Бардюков <ferisefbns@gmail.com>
2019-06-14 21:48:42 +0300

    Add logo as svg tag

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,8 +6,6 @@ import action from '../../actions/login';
 
 import styles from './header.module.css';
 
-import logo from './logo.svg';
-
 class Header extends React.Component {
   logout() {
     return (e) => {
@@ -29,8 +27,8 @@ class Header extends React.Component {
         <div className={styles.container}>
           <div className={styles.left}>
             <Link className={styles.logo} to="/">
-              <svg>
-                <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref={`${logo}#logo`} />
+              <svg viewBox="0 0 48 48">
+                <path d="M24 2C14.06 2 6 10.06 6 20v14c0 3.31 2.69 6 6 6h6V24h-8v-4c0-7.73 6.27-14 14-14s14 6.27 14 14v4h-8v16h8v2H24v4h12c3.31 0 6-2.69 6-6V20c0-9.94-8.06-18-18-18z" />
               </svg>
             </Link>
             {token && (
