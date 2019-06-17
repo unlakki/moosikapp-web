7bf8847a206ad2ce0e969442f84b0a438cf50ca2
Василий Бардюков <ferisefbns@gmail.com>
2019-06-17 17:49:54 +0300

    Remove errors localy

--- src/components/Main/components/Status/index.jsx
+++ src/components/Main/components/Status/index.jsx
@@ -10,7 +10,7 @@ class Status extends React.Component {
     super(props);
 
     this.state = {
-      storage: null, error: null,
+      storage: null,
     };
   }
 
@@ -37,32 +37,33 @@ class Status extends React.Component {
 
         this.setState({ storage });
       })
-      .catch(error => this.setState({ error: error.toString() }));
+      .catch((error) => {
+        // Global Error Message
+      });
   }
 
   render() {
-    const { storage, error } = this.state;
+    const { storage } = this.state;
 
     return (
       <section className={styles.status}>
-        <h1 className={styles.head}>Storage Status</h1>
+        <h1 className={styles.title}>Storage Status</h1>
         {storage && (
           <div className={styles.body}>
-            <p className={styles.prop}>
+            <p className={styles.item}>
               Total Space:&ensp;
               <span>{filesize(storage.totalSpace)}</span>
             </p>
-            <p className={styles.prop}>
+            <p className={styles.item}>
               Used Space:&ensp;
               <span>{filesize(storage.usedSpace)}</span>
             </p>
-            <p className={styles.prop}>
+            <p className={styles.item}>
               Max File Size:&ensp;
               <span>{filesize(storage.maxFileSize)}</span>
             </p>
           </div>
         )}
-        {error && <p className={styles.error}>{error}</p>}
       </section>
     );
   }
