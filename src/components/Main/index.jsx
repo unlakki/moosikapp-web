e8c858dc3b137ea89c640aa8beb2f601d6ad0301
Василий Бардюков <ferisefbns@gmail.com>
2019-07-28 21:06:18 +0300

    Test test array

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -3,12 +3,27 @@ import Article from './Article';
 
 import styles from './layouts/Main.module.css';
 
+const lyrics = [
+  'А знаешь все мечты такие сладкие на вкус...и цвет.',
+  'И выше неба ты мне улыбнись, я улыбнусь в ответ.',
+  'Давай полетаем что ли,',
+  'Это так легко ли,',
+  'Не волнуйся я держу.',
+  'Давай про гуляем школу',
+  'Обещаю что я',
+  'Никому не расскажу.',
+  'Давай полетаем что ли,',
+  'Облака сквозь шторы, как обертки от конфет.',
+  'Давай про гуляем школу,',
+  'И закажем с колой кучу счастья на десерт.',
+];
+
 const Main = () => (
   <div className={styles.articles}>
     <Article
-      title="Nya?"
-      text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit natus aspernatur, cumque nulla ab voluptate exercitationem repudiandae, illum officiis dicta architecto provident nam eos voluptatibus consequuntur veniam consequatur. Fugit, sunt."
-      image="https://cdn.moosikapp.tk/v1/15ba5da8a27ee9565c2c9636bad4d994/azunyan.jpg"
+      title="Nya? Nya!"
+      text={lyrics}
+      attachment="https://cdn.moosikapp.tk/v1/15ba5da8a27ee9565c2c9636bad4d994/azunyan.jpg"
     />
   </div>
 );
