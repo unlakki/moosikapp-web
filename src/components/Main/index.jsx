5cf1895f3683ea57754dc44c1e7ef6313d85b202
Василий Бардюков <ferisefbns@gmail.com>
2019-08-05 19:20:51 +0300

    Add page title

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -3,28 +3,23 @@ import Article from './Article';
 
 import styles from './layouts/Main.module.css';
 
-const lyrics = [
-  'А знаешь все мечты такие сладкие на вкус...и цвет.',
-  'И выше неба ты мне улыбнись, я улыбнусь в ответ.',
-  'Давай полетаем что ли,',
-  'Это так легко ли,',
-  'Не волнуйся я держу.',
-  'Давай про гуляем школу',
-  'Обещаю что я',
-  'Никому не расскажу.',
-  'Давай полетаем что ли,',
-  'Облака сквозь шторы, как обертки от конфет.',
-  'Давай про гуляем школу,',
-  'И закажем с колой кучу счастья на десерт.',
+const lines = [
+  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus hic rem illum. Consequatur nihil rem laboriosam at excepturi impedit, itaque quam nemo libero eum. Omnis cum ipsam quod voluptatibus! Cum.',
+  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus hic rem illum. Consequatur nihil rem laboriosam at excepturi impedit, itaque quam nemo libero eum. Omnis cum ipsam quod voluptatibus! Cum.',
 ];
 
 const Main = () => (
-  <div className={styles.articles}>
-    <Article
-      title="Nya? Nya!"
-      text={lyrics}
-      attachment="https://cdn.moosikapp.tk/v1/15ba5da8a27ee9565c2c9636bad4d994/azunyan.jpg"
-    />
+  <div className={styles.container}>
+    <h1 className={styles.title}>Updates</h1>
+    <div className={styles.articles}>
+      <Article
+        title="Lorem ipsum."
+        text={lines}
+        attachment="https://cdn.moosikapp.tk/v1/15ba5da8a27ee9565c2c9636bad4d994/azunyan.jpg"
+        author={{ name: 'Hibikaze', image: 'https://cdn.moosikapp.tk/v1/15ba5da8a27ee9565c2c9636bad4d994/azunyan.jpg' }}
+        date={new Date()}
+      />
+    </div>
   </div>
 );
 
