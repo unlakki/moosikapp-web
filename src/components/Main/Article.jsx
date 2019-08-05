a36991c67440b11ea486834f79c05999745f5c19
Василий Бардюков <ferisefbns@gmail.com>
2019-08-05 19:20:29 +0300

    update styles. Add author and date

--- src/components/Main/Article.jsx
+++ src/components/Main/Article.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import uuidv4 from 'uuid/v4';
+import moment from 'moment';
 
 import styles from './layouts/Article.module.css';
 
-const Article = ({ title, text, attachment }) => (
+const Article = ({
+  title, text, attachment, author, date,
+}) => (
   <article className={styles.container}>
-    <h1 className={styles.title}>{title}</h1>
+    <div className={styles.header}>
+      <div className={styles.meta}>
+        <h2 className={styles.title}>{title}</h2>
+        <span className={styles.date}>{moment(date).fromNow()}</span>
+      </div>
+      <div className={styles.author}>
+        <span className={styles.authorName}>{author.name}</span>
+        <img className={styles.authorImage} src={author.image} alt={author.name} />
+      </div>
+    </div>
     <div className={styles.content}>
+      {attachment && <img className={styles.attachment} src={attachment} alt={title} />}
       {(typeof text === 'string') && <p>{text}</p>}
       {(typeof text === 'object') && text.map(block => <p key={uuidv4()}>{block}</p>)}
-      <img src={attachment} alt={title} />
     </div>
   </article>
 );
@@ -26,6 +38,11 @@ Article.propTypes = {
     PropTypes.arrayOf(PropTypes.string),
   ]).isRequired,
   attachment: PropTypes.string,
+  author: PropTypes.shape({
+    name: PropTypes.string,
+    image: PropTypes.string,
+  }).isRequired,
+  date: PropTypes.number.isRequired,
 };
 
 export default Article;
