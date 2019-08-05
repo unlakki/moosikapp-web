77c025475258693dbc337ddf851f97a1afd4bd1f
Василий Бардюков <ferisefbns@gmail.com>
2019-08-05 19:54:50 +0300

    Update test article

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -3,22 +3,28 @@ import Article from './Article';
 
 import styles from './layouts/Main.module.css';
 
-const lines = [
-  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus hic rem illum. Consequatur nihil rem laboriosam at excepturi impedit, itaque quam nemo libero eum. Omnis cum ipsam quod voluptatibus! Cum.',
-  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus hic rem illum. Consequatur nihil rem laboriosam at excepturi impedit, itaque quam nemo libero eum. Omnis cum ipsam quod voluptatibus! Cum.',
-];
+const article = {
+  title: 'Lorem ipsum.',
+  content: [
+    'https://pp.userapi.com/c855736/v855736665/b09b5/EOFLOjVWWig.jpg',
+    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus hic rem illum.',
+    'https://sun9-18.userapi.com/c857524/v857524371/30b22/6z81CRl1-jk.jpg',
+    'Consequatur nihil rem laboriosam at excepturi impedit, itaque quam nemo libero eum.',
+    'Omnis cum ipsam quod voluptatibus! Cum.',
+  ],
+  author: {
+    username: 'Hibikaze',
+    image: 'https://pp.userapi.com/c850120/v850120001/1ad28c/F35G1gFJG24.jpg',
+  },
+  date: Date.now(),
+};
+
 
 const Main = () => (
   <div className={styles.container}>
     <h1 className={styles.title}>Updates</h1>
     <div className={styles.articles}>
-      <Article
-        title="Lorem ipsum."
-        text={lines}
-        attachment="https://cdn.moosikapp.tk/v1/15ba5da8a27ee9565c2c9636bad4d994/azunyan.jpg"
-        author={{ name: 'Hibikaze', image: 'https://cdn.moosikapp.tk/v1/15ba5da8a27ee9565c2c9636bad4d994/azunyan.jpg' }}
-        date={new Date()}
-      />
+      <Article {...article} />
     </div>
   </div>
 );
