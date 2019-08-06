72a5aa76f2ac693abb56cfe6fb6fb34cf9fc3a07
Василий Бардюков <ferisefbns@gmail.com>
2019-08-07 00:08:14 +0300

    Remove title

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -3,12 +3,27 @@ import Article from './Article';
 
 import styles from './layouts/Main.module.css';
 
-const article = {
+const article1 = {
   title: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
   content: [
-    'https://pp.userapi.com/c855736/v855736665/b09b5/EOFLOjVWWig.jpg',
+    'https://sun1-16.userapi.com/c848124/u476882832/docs/d12/039811b6e945/giphy.gif',
     'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus hic rem illum.',
-    'https://sun9-18.userapi.com/c857524/v857524371/30b22/6z81CRl1-jk.jpg',
+    'Consequatur nihil rem laboriosam at excepturi impedit, itaque quam nemo libero eum.',
+    'https://pp.userapi.com/c858328/v858328601/2dce7/PTZSqKR2eg8.jpg',
+    'Omnis cum ipsam quod voluptatibus! Cum.',
+  ],
+  author: {
+    username: 'Hibikaze',
+    image: 'https://pp.userapi.com/c850120/v850120001/1ad28c/F35G1gFJG24.jpg',
+  },
+  date: Date.now(),
+};
+
+const article2 = {
+  title: 'Lorem ipsum.',
+  content: [
+    'https://sun9-6.userapi.com/c850220/v850220268/19d5f8/RIJmY1ish6g.jpg',
+    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus hic rem illum.',
     'Consequatur nihil rem laboriosam at excepturi impedit, itaque quam nemo libero eum.',
     'Omnis cum ipsam quod voluptatibus! Cum.',
   ],
@@ -22,10 +37,8 @@ const article = {
 
 const Main = () => (
   <div className={styles.container}>
-    <h1 className={styles.title}>News</h1>
-    <div className={styles.articles}>
-      <Article {...article} />
-    </div>
+    <Article {...article1} />
+    <Article {...article2} />
   </div>
 );
 
