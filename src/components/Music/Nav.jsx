8470edbe07f90907f5d89c66c53c2e42c777818d
Василий Бардюков <ferisefbns@gmail.com>
2019-08-07 13:58:14 +0300

    Using routes

--- src/components/Music/Nav.jsx
+++ src/components/Music/Nav.jsx
@@ -1,43 +1,23 @@
 import React from 'react';
-import uuidv4 from 'uuid/v4';
 import { Link } from 'react-router-dom';
+import routes from './routes';
 
 import styles from './layouts/Nav.module.css';
 
-const iterate = items => items.map(({ uuid, path, name }) => {
-  const active = window.location.pathname === path;
-  return (
-    <Link
-      key={uuid}
-      className={`${styles.item} ${active ? styles.active : ''}`.trim()}
-      to={path}
-    >
-      {name}
-    </Link>
-  );
-});
-
 const Nav = () => (
   <nav className={styles.nav}>
-    {iterate(
-      [
-        {
-          uuid: uuidv4(),
-          path: '/music',
-          name: 'All',
-        },
-        {
-          uuid: uuidv4(),
-          path: '/music/favorites',
-          name: 'Likes',
-        },
-        {
-          uuid: uuidv4(),
-          path: '/music/search',
-          name: 'Search',
-        },
-      ],
-    )}
+    {routes.map(({ uuid, path, name }) => {
+      const active = window.location.pathname === path;
+      return (
+        <Link
+          key={uuid}
+          className={`${styles.item}${active ? ` ${styles.active}` : ''}`}
+          to={path}
+        >
+          {name}
+        </Link>
+      );
+    })}
   </nav>
 );
 
