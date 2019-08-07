c9f1cd4657347b4e405ba6e193a53556d5fc0aab
Василий Бардюков <ferisefbns@gmail.com>
2019-08-07 15:36:07 +0300

    Compose with redux DevTools

--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/root';
 
 export default createStore(
   rootReducer,
-  applyMiddleware(thunk),
+  composeWithDevTools(applyMiddleware(thunk)),
 );
