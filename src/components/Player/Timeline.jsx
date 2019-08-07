596170a53562b08dfa13c534b583abf3b5da1f4c
Василий Бардюков <ferisefbns@gmail.com>
2019-08-07 15:02:52 +0300

    Write as pure functions

--- src/components/Player/Timeline.jsx
+++ src/components/Player/Timeline.jsx
@@ -1,49 +1,43 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 
 import styles from './layouts/Timeline.module.css';
 
-class Timeline extends PureComponent {
-  onClick(e) {
-    const { duration, onChange } = this.props;
-
-    const r = e.currentTarget.getBoundingClientRect();
-    const value = (e.clientX - r.left) / r.width;
-
-    onChange(duration * value);
-  }
-
-  render() {
-    const { currentTime, duration } = this.props;
-
-    return (
-      <div className={styles.timeline}>
-        <div className={styles.timePassed}>{moment(currentTime * 1000).format('mm:ss')}</div>
-        <div
-          className={styles.progressWrapper}
-          role="slider"
-          aria-valuemax={duration}
-          aria-valuemin={0}
-          aria-valuenow={currentTime}
-          tabIndex={-1}
-          onKeyDown={null}
-          onClick={this.onClick.bind(this)}
-        >
-          <div className={styles.progressBackground}>
-            <div className={styles.progressBar} style={{ width: `${currentTime / duration * 100}%` }} />
-          </div>
-        </div>
-        <div className={styles.duration}>{moment(duration * 1000).format('mm:ss')}</div>
+const Timeline = ({ currentTime, duration, onChange }) => (
+  <div className={styles.timeline}>
+    <div className={styles.timePassed}>{moment(currentTime * 1000).format('mm:ss')}</div>
+    <div
+      className={styles.progressWrapper}
+      role="slider"
+      aria-valuemax={duration}
+      aria-valuemin={0}
+      aria-valuenow={currentTime}
+      tabIndex={-1}
+      onKeyDown={null}
+      onClick={(e) => {
+        const r = e.currentTarget.getBoundingClientRect();
+        if (typeof onChange === 'function') {
+          onChange(duration * (e.clientX - r.left) / r.width);
+        }
+      }}
+    >
+      <div className={styles.progressBackground}>
+        <div className={styles.progressBar} style={{ width: `${currentTime / duration * 100}%` }} />
       </div>
-    );
-  }
-}
+    </div>
+    <div className={styles.duration}>{moment(duration * 1000).format('mm:ss')}</div>
+  </div>
+);
+
+Timeline.defaultProps = {
+  onChange: null,
+};
 
 Timeline.propTypes = {
   currentTime: PropTypes.number.isRequired,
   duration: PropTypes.number.isRequired,
-  onChange: PropTypes.func.isRequired,
+  onChange: PropTypes.func,
 };
 
 export default Timeline;
--- src/components/Player/VolumeSlider.jsx
+++ src/components/Player/VolumeSlider.jsx
@@ -1,51 +1,47 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './layouts/VolumeSlider.module.css';
 
-class VolumeSlider extends PureComponent {
-  constructor(props) {
-    super(props);
+const VolumeSlider = ({ value, onChange }) => {
+  const [volume, setVolume] = useState(value);
 
-    this.state = { value: props.value };
-  }
+  const updateVolume = (v) => {
+    setVolume(v);
+    if (typeof onChange === 'function') {
+      onChange(v);
+    }
+  };
 
-  onClick(e) {
-    const { onChange } = this.props;
-
-    const r = e.currentTarget.getBoundingClientRect();
-    const value = 1 - (e.clientY - r.top) / r.height;
-
-    this.setState({ value });
-    onChange(value);
-  }
-
-  render() {
-    const { value } = this.state;
-
-    return (
-      <div className={styles.wrapper}>
-        <div
-          className={styles.slider}
-          role="slider"
-          aria-valuemax={1}
-          aria-valuemin={0}
-          aria-valuenow={value}
-          tabIndex={-1}
-          onKeyDown={null}
-          onClick={this.onClick.bind(this)}
-        >
-          <div className={styles.background} />
-          <div className={styles.bar} style={{ height: `${100 * value}%` }} />
-        </div>
+  return (
+    <div className={styles.wrapper}>
+      <div
+        className={styles.slider}
+        role="slider"
+        aria-valuemax={1}
+        aria-valuemin={0}
+        aria-valuenow={value}
+        tabIndex={-1}
+        onKeyDown={null}
+        onClick={(e) => {
+          const r = e.currentTarget.getBoundingClientRect();
+          updateVolume(1 - (e.clientY - r.top) / r.height);
+        }}
+      >
+        <div className={styles.background} />
+        <div className={styles.bar} style={{ height: `${100 * volume}%` }} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+VolumeSlider.defaultProps = {
+  onChange: null,
+};
 
 VolumeSlider.propTypes = {
   value: PropTypes.number.isRequired,
-  onChange: PropTypes.func.isRequired,
+  onChange: PropTypes.func,
 };
 
 export default VolumeSlider;
