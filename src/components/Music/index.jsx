b7d4ac76565234be80f3d0ed407f91419eacb788
Василий Бардюков <ferisefbns@gmail.com>
2019-08-07 13:57:58 +0300

    Add pagination block

--- src/components/Music/index.jsx
+++ src/components/Music/index.jsx
@@ -8,11 +8,14 @@ import styles from './layouts/Music.module.css';
 const Music = () => (
   <div className={styles.container}>
     <Nav />
-    {routes.map(({
-      uuid, path, component, exact,
-    }) => (
-      <Route key={uuid} path={path} component={component} exact={exact} />
-    ))}
+    {routes.map(route => <Route key={route.uuid} {...route} />)}
+    <div className={styles.pagination}>
+      <button type="button">1</button>
+      <button type="button">2</button>
+      <button type="button">3</button>
+      <button type="button">4</button>
+      <button type="button">5</button>
+    </div>
   </div>
 );
 
