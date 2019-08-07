29ac70f3ce5844cfbbfe4eb469204782fbe7a8d7
Василий Бардюков <ferisefbns@gmail.com>
2019-08-07 15:36:51 +0300

    Comment pagination

--- src/components/Music/index.jsx
+++ src/components/Music/index.jsx
@@ -9,13 +9,13 @@ const Music = () => (
   <div className={styles.container}>
     <Nav />
     {routes.map(route => <Route key={route.uuid} {...route} />)}
-    <div className={styles.pagination}>
+    {/* <div className={styles.pagination}>
       <button type="button">1</button>
       <button type="button">2</button>
       <button type="button">3</button>
       <button type="button">4</button>
       <button type="button">5</button>
-    </div>
+    </div> */}
   </div>
 );
 
