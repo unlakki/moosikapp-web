0a293d6c971179d0e4a8fa6dab455c7bda0eefc2
Василий Бардюков <ferisefbns@gmail.com>
2019-08-07 23:13:32 +0300

    Hide Button during uploading

--- src/components/Upload/index.jsx
+++ src/components/Upload/index.jsx
@@ -35,40 +35,42 @@ const Upload = () => {
 
   return (
     <div className={styles.container}>
-      <div
-        className={`${styles.form}${drag ? ` ${styles.drop}` : ''}`}
-        onDrop={(e) => {
-          e.preventDefault();
-          upload(Object.values(e.dataTransfer.files));
-          setDrag(false);
-        }}
-        onDragOver={(e) => {
-          e.preventDefault();
-          setDrag(true);
-        }}
-        onDragLeave={(e) => {
-          e.preventDefault();
-          setDrag(false);
-        }}
-      >
-        <h1 className={styles.title}>Drag and drop your tracks here</h1>
-        <label htmlFor={uuids.input} className={styles.file}>
-          <input
-            id={uuids.input}
-            className={styles.input}
-            type="file"
-            accept="audio/mpeg"
-            multiple
-            onChange={(e) => {
-              upload(Object.values(e.target.files));
-            }}
-          />
-          <span>or choose files to upload</span>
-        </label>
-        <div className={styles.note}>
-          <p>Your audio file may not exceed 10 MB and has to be in MP3 format.</p>
+      {(files.length === 0) && (
+        <div
+          className={`${styles.form}${drag ? ` ${styles.drop}` : ''}`}
+          onDrop={(e) => {
+            e.preventDefault();
+            upload(Object.values(e.dataTransfer.files));
+            setDrag(false);
+          }}
+          onDragOver={(e) => {
+            e.preventDefault();
+            setDrag(true);
+          }}
+          onDragLeave={(e) => {
+            e.preventDefault();
+            setDrag(false);
+          }}
+        >
+          <h1 className={styles.title}>Drag and drop your tracks here</h1>
+          <label htmlFor={uuids.input} className={styles.file}>
+            <input
+              id={uuids.input}
+              className={styles.input}
+              type="file"
+              accept="audio/mpeg"
+              multiple
+              onChange={(e) => {
+                upload(Object.values(e.target.files));
+              }}
+            />
+            <span>or choose files to upload</span>
+          </label>
+          <div className={styles.note}>
+            <p>Your audio file may not exceed 10 MB and has to be in MP3 format.</p>
+          </div>
         </div>
-      </div>
+      )}
       {(files && files.length > 0) && (
         <div className={styles.files}>
           {Object.values(files).map(file => (
