7cbde1f56e5d742619b490a82a5ec66e4ff66ced
Василий Бардюков <ferisefbns@gmail.com>
2019-08-07 22:59:13 +0300

    Update image

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -6,7 +6,7 @@ import styles from './layouts/Main.module.css';
 const article1 = {
   title: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
   content: [
-    'https://sun1-16.userapi.com/c848124/u476882832/docs/d12/039811b6e945/giphy.gif',
+    'https://sun9-24.userapi.com/c858328/v858328601/2dcf1/64dBzUz-gO0.jpg',
     'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus hic rem illum.',
     'Consequatur nihil rem laboriosam at excepturi impedit, itaque quam nemo libero eum.',
     'https://pp.userapi.com/c858328/v858328601/2dce7/PTZSqKR2eg8.jpg',
