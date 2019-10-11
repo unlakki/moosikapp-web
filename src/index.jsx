f35bd5e30e7a469bd79057405817109bff9da4ab
Ferisef <ferisefbns@gmail.com>
2019-10-11 18:19:56 +0300

    Add homepage

--- src/index.jsx
+++ src/index.jsx
@@ -2,22 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import Home from './pages';
 import store from './store';
 import * as serviceWorker from './serviceWorker';
-import App from './components/App';
-import Sidebar from './components/Sidebar';
-import Modal from './components/Modal';
 
 import './layouts/default.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <Sidebar />
-      <App />
-      <Modal />
-    </Provider>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
 
