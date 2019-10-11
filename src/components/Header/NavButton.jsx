bd6cca596b3b69200874e777ef3bab0d93f03b45
Ferisef <ferisefbns@gmail.com>
2019-10-12 01:17:41 +0300

    Logo -> Icon

--- src/components/Header/NavButton.jsx
+++ src/components/Header/NavButton.jsx
@@ -13,7 +13,7 @@ const Button = styled.button`
   background: transparent;
 `;
 
-const Logo = styled.svg`
+const Icon = styled.svg`
   fill: var(--light);
   transition: fill .2s ease;
 
@@ -33,11 +33,11 @@ const NavButton = ({ hidden, show, hide }) => (
       hide();
     }}
   >
-    <Logo viewBox="0 0 24 24">
+    <Icon viewBox="0 0 24 24">
       <path
         d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"
       />
-    </Logo>
+    </Icon>
   </Button>
 );
 
