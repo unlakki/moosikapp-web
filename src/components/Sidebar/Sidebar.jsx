3ea0df1efa704607dcaade45ca7bc0dba9aa8e08
Ferisef <ferisefbns@gmail.com>
2019-10-11 19:06:29 +0300

    Connect to redux store

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import css from './css/Sidebar.module.css';
 
-export default () => (
-  <div id="sidebar" className={css.wrapper} style={{ display: 'none' }} />
+const Sidebar = ({ hidden }) => (
+  <div className={classnames(css.wrapper, { [css.hidden]: hidden })} />
 );
+
+Sidebar.propTypes = {
+  hidden: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = store => ({
+  hidden: store.sidebar.hidden,
+});
+
+export default connect(mapStateToProps)(Sidebar);
