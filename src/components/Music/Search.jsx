348fc32cb25cd81c315c49506e348dfe8b56e7c5
Ferisef <ferisefbns@gmail.com>
2019-10-12 14:15:40 +0300

    Add search

--- src/components/Music/Search.jsx
+++ src/components/Music/Search.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import Music from './Music';
 
+import css from './css/Music.module.css';
+
 const Search = () => (
   <Music>
-    <div>Search</div>
+    <form className={css.searchForm}>
+      <input className={css.input} type="text" placeholder="Search" />
+      <input className={css.submit} type="submit" value="Search" />
+    </form>
+    <div className={css.songs}>Search</div>
   </Music>
 );
 
