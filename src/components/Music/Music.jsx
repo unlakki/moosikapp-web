181aac684cba45885807c32995e828e79873fac0
Ferisef <ferisefbns@gmail.com>
2019-10-12 17:50:47 +0300

    Require login

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Subnav from './Subnav';
 
 import css from './css/Music.module.css';
 
-const Music = ({ children }) => (
-  <section className={css.section}>
-    <Subnav />
-    <div className={css.wrapper}>
-      {children}
-    </div>
-  </section>
-);
+const Music = ({ children, token, history }) => {
+  useEffect(() => {
+    if (!token) {
+      history.push('/');
+    }
+  });
+
+
+  return (
+    <section className={css.section}>
+      <Subnav />
+      <div className={css.wrapper}>
+        {children}
+      </div>
+    </section>
+  );
+};
 
 Music.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.element,
     PropTypes.arrayOf(PropTypes.element),
   ]).isRequired,
+  token: PropTypes.string.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
 };
 
-export default Music;
+const mapStateToProps = store => ({
+  token: store.login.token,
+});
+
+export default withRouter(connect(mapStateToProps)(Music));
